5191110ce554f2bd6882b6a30f2c03b0bfb90fb1
jorgechavez99 <jorgeluischavez.jlcm@gmail.com>
2024-02-05 02:51:03 +0100

    done: recuperar contraseña

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,19 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
-const {registrarUsuario, getLogin,getRegistro, loguearUsuario}=require('../controllers/authControllers')
+const {registrarUsuario, getLogin,getRegistro, loguearUsuario,logoutUsuario,authGoogle,getRecuperarPass,postRecuperarPass}=require('../controllers/authControllers')
 
 //Vista del login 
 router.get("/",getLogin)
 //envia email y password para comprobar 
 router.post("/", loguearUsuario)
 
+//Logout
+router.post("/logout",logoutUsuario)
+
+
+//Autenticacion por Google
+router.post("/authGoogle",authGoogle)
 
 //Muestra el panel de registro
 router.get("/registro",getRegistro)
 
 //registrar usuario atravez de firebase
 router.post("/registro", registrarUsuario)
+
+//Recuperar Contraseña
+router.get("/recuperarPass",getRecuperarPass)
+router.post("/recuperarPass",postRecuperarPass)
 
 
 module.exports=router
